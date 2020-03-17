100a1499284b77d59c937efcdb9cf01a0cba4524
aishwarydhare <aishwarydhare@gmail.com>
2020-03-17 18:34:03 +0530

    changes for web app integration todo: webhook auth check, failure, and logging

--- app.py
+++ app.py
@@ -3,9 +3,14 @@ from flask import Flask, request, render_template
 import requests
 import razorpay
 import json
-from secrets import auth_key, DRF_SERVER_URL, HOST, PORT, RAZORPAY_APP_ID, RAZORPAY_APP_SECRET
+from secrets import CLIENT_ID, CLIENT_SECRET, DRF_SERVER_URL, HOST, PORT, RAZORPAY_APP_ID, RAZORPAY_APP_SECRET
+
 razorpay_client = razorpay.Client(auth=(RAZORPAY_APP_ID, RAZORPAY_APP_SECRET))
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+drf_endpoint_headers = {
+    'Authorisation': CLIENT_ID + "-" + CLIENT_SECRET,
+}
 
 
 @app.route('/payment/success', methods=['POST'])
@@ -21,32 +26,40 @@ def payment_success():
     } 
     '''
 
-    headers = {
-        'authkey': auth_key
-    }
-
-    url = DRF_SERVER_URL + "/api/android/v1/payment/success"
-    ret = requests.post(url, headers=headers, data=payment_success_data)
+    url = DRF_SERVER_URL + "/api/android/v1/payment/verify"
+    ret = requests.post(url, headers=drf_endpoint_headers, data={
+        'razor_order_id': payment_success_data['razorpay_order_id'],
+        'razor_payment_id': payment_success_data['razorpay_payment_id'],
+        'razor_signature': payment_success_data['razorpay_signature'],
+    })
     status_code = ret.status_code
     # todo: handle if response is not 200 return something
-    # if status_code is 200:
-    return render_template('payment_success.html')
+    # todo: logging
+    host = "http://%s:%s/" % (request.remote_addr, str(request.environ['REMOTE_PORT']))
+    if status_code is 200:
+        return render_template('payment_success.html', host=host)
+    else:
+        # todo: logging
+        return render_template('payment_fail.html', host=host), 500
 
 
 @app.route('/payment/fail', methods=['POST'])
 def payment_fail():
-    headers = {
-        'authkey': auth_key
-    }
+    # todo: validate auth key of webhook
     data_in_bytes = request.data
     payment_fail_decoded_data = json.loads(data_in_bytes.decode("utf-8"))
     payment_fail_data = payment_fail_decoded_data['payload']['payment']['entity']
+    print(payment_fail_data)
     url = DRF_SERVER_URL + "/api/android/v1/payment/fail"
-    ret = requests.post(url, headers=headers, data=payment_fail_data)
+    ret = requests.post(url, headers=drf_endpoint_headers, data=payment_fail_data)
     status_code = ret.status_code
     # todo: handle if response is not 200 return something
+    # todo: logging
+    host = "http://%s:%s/" % (request.remote_addr, str(request.environ['REMOTE_PORT']))
     if status_code is 200:
-        return render_template('payment_fail.html')
+        return render_template('payment_fail.html', host=host), 500
+    else:
+        return render_template('payment_fail.html', host=host), 500
 
 
 if __name__ == '__main__':
